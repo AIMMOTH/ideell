03ea2a028a2e26bafa062fb8eb7b06c182ea6347
Carl <Carl@viting>
2021-03-28 09:29:54 +0200

    Clean up, using https, trying base href for angular

--- www/vi/src/app/app.module.ts
+++ www/vi/src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { APP_BASE_HREF } from '@angular/common';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,9 @@ import { AppComponent } from './app.component';
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [
+    {provide: APP_BASE_HREF, useValue: '/vi/'}
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
