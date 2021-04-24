f4020a9043e39f88cd9fd19546268eeb1fc29d2f
Carl <Carl@viting>
2021-04-24 11:41:53 +0200

    Spring boot security 5 with JWT and CORS (any)

---
+++ api/src/main/java/eu/ideell/api/spring/AuthApi.java
@@ -0,0 +1,20 @@
+package eu.ideell.api.spring;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin("*")
+public class AuthApi {
+
+  @GetMapping("/api/v1/public/")
+  public String getPublic() {
+    return "public pong";
+  }
+
+  @GetMapping("/api/v1/private/")
+  public String getPrivate() {
+    return "private pong";
+  }
+}
